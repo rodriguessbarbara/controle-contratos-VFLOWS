0d81f50fd0187a04f6c6461d290876d8d22e3b44
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-01-31 20:43:02 -0300

    feat: :construction: iniciado function handleNext e sendData

--- src/components/Detalhes/Detalhes.js
+++ src/components/Detalhes/Detalhes.js
@@ -4,6 +4,8 @@ import { Form } from "@unform/web";
 import Input from "../Form/Input";
 import { UserContext } from "../../UserContext";
 import Footer from "../Footer";
+import Button from "../Form/Button";
+import { useNavigate } from "react-router-dom";
 
 const Detalhes = () => {
   const [value, setValue] = useState("");
@@ -11,11 +13,23 @@ const Detalhes = () => {
   const [message, setMessage] = useState(null);
   const { data, dataContracts, contract } = useContext(UserContext);
 
+  const navigate = useNavigate();
+
   // function onChange({ target }) {
   //   setMessage(null);
   //   setErro(null);
   //   setValue(target.value);
   // }
+
+  function handleNext() {
+    sendData();
+  }
+
+  function sendData() {
+    console.log('handlenext');
+    window.alert("Solicitação 999999 foi enviada com sucesso.");
+    navigate("/");
+  }
 
   return (
     <div className="flex items-center justify-center">
@@ -67,6 +81,15 @@ const Detalhes = () => {
           </Form>
 
           <input label="Retenção de Impostos" type="checkbox" />
+        </div>
+
+        <div className="flex gap-16 ml-8 mr-8 mb-10">
+          <Button color="bg-yellow-500" borderColor="shadow-yellow-600/80" onClick={() => navigate("/contratos")}>
+            Anterior
+          </Button>
+          <Button color="bg-lime-700" borderColor="shadow-lime-600/80" onClick={handleNext}>
+            Próximo
+          </Button>
         </div>
 
         <Footer />
