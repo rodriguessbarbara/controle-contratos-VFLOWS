1a5ee2131ebe0a451b768d263764e511b017553e
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-02-01 14:25:46 -0300

    feat: :construction: layoult e estilizacao da pagina Detalhes

--- src/components/Detalhes/Detalhes.js
+++ src/components/Detalhes/Detalhes.js
@@ -11,22 +11,37 @@ const Detalhes = () => {
   const [value, setValue] = useState("");
   const [erro, setErro] = useState(null);
   const [message, setMessage] = useState(null);
-  const { data, dataContracts, contract } = useContext(UserContext);
+  const { dataContracts, contract } = useContext(UserContext);
+  const [showImpostos, setShowImpostos] = useState(false);
+  const [showTecnica, setShowTecnica] = useState(false);
 
   const navigate = useNavigate();
 
-  // function onChange({ target }) {
-  //   setMessage(null);
-  //   setErro(null);
-  //   setValue(target.value);
-  // }
+  function handleChange({ target }) {
+    setMessage(null);
+    setErro(null);
+
+    if (target.checked) {
+      setShowImpostos(true);
+    } else {
+      setShowImpostos(false);
+    }
+
+    if (showImpostos) {
+      if (target.checked) {
+        setShowTecnica(true);
+      } else {
+        setShowTecnica(false);
+      }
+    }
+  }
 
   function handleNext() {
     sendData();
   }
 
   function sendData() {
-    console.log('handlenext');
+    console.log("handlenext");
     window.alert("Solicitação 999999 foi enviada com sucesso.");
     navigate("/");
   }
@@ -80,16 +95,136 @@ const Detalhes = () => {
             />
           </Form>
 
-          <input label="Retenção de Impostos" type="checkbox" />
-        </div>
+          <Form>
+            <Input
+              label="Retenção de Impostos"
+              type="checkbox"
+              name="retencao"
+              erro={erro}
+              message={message}
+              onChange={handleChange}
+            />
+          </Form>
 
-        <div className="flex gap-16 ml-8 mr-8 mb-10">
-          <Button color="bg-yellow-500" borderColor="shadow-yellow-600/80" onClick={() => navigate("/contratos")}>
-            Anterior
-          </Button>
-          <Button color="bg-lime-700" borderColor="shadow-lime-600/80" onClick={handleNext}>
-            Próximo
-          </Button>
+          {showImpostos ? (
+            <>
+              <div className="border-2 rounded border-red-300 p-2 ml-5 mr-5 mb-2">
+                <Form className="inline-flex flex-wrap gap-2 justify-around mb-8">
+                  <Input
+                    label="ISSQN"
+                    type="number"
+                    step=".01"
+                    name="ISSQN"
+                    erro={erro}
+                    message={message}
+                  />
+
+                  <Input
+                    label="IIRRF"
+                    type="number"
+                    step=".01"
+                    name="IIRRF"
+                    erro={erro}
+                    message={message}
+                  />
+
+                  <Input
+                    label="CSLL"
+                    type="number"
+                    step=".01"
+                    name="CSLL”"
+                    erro={erro}
+                    message={message}
+                  />
+
+                  <Input
+                    label="COFINS"
+                    type="number"
+                    step=".01"
+                    name="COFINS"
+                    erro={erro}
+                    message={message}
+                  />
+
+                  <Input
+                    label="INSS"
+                    type="number"
+                    step=".01"
+                    name="INSS"
+                    erro={erro}
+                    message={message}
+                  />
+
+                  <Input
+                    label="PIS"
+                    type="number"
+                    step=".01"
+                    name="PIS”"
+                    erro={erro}
+                    message={message}
+                  />
+                </Form>
+              </div>
+
+              <Form>
+                <Input
+                  label="Retenção Técnica"
+                  type="checkbox"
+                  name="retencao"
+                  erro={erro}
+                  message={message}
+                  onChange={handleChange}
+                />
+              </Form>
+            </>
+          ) : (
+            ""
+          )}
+
+          {showImpostos && showTecnica ? (
+            <>
+              <div className="border-2 rounded border-red-300 p-2 ml-5 mr-5 mb-2">
+                <Form className="inline-flex flex-wrap gap-2 justify-between mb-8">
+                  <Input
+                    label="Valor"
+                    type="number"
+                    step=".01"
+                    name="Valor"
+                    erro={erro}
+                    message={message}
+                  />
+
+                  <Input
+                    label="Percentual"
+                    type="number"
+                    step=".01"
+                    name="Percentual"
+                    erro={erro}
+                    message={message}
+                  />
+                </Form>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
+
+          <div className="flex gap-16 ml-8 mr-8 mb-10">
+            <Button
+              color="bg-yellow-500"
+              borderColor="shadow-yellow-600/80"
+              onClick={() => navigate("/contratos")}
+            >
+              Anterior
+            </Button>
+            <Button
+              color="bg-lime-700"
+              borderColor="shadow-lime-600/80"
+              onClick={handleNext}
+            >
+              Próximo
+            </Button>
+          </div>
         </div>
 
         <Footer />
